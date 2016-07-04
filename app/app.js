9b300cd77ee9b1f2e4f0d857f47e47abc54897d2
nimrod bar-on <nimrodbaron1@gmail.com>
2016-07-04 20:40:40 +0300

    4.7.16 counter is working

--- app/app.js
+++ app/app.js
@@ -2,21 +2,41 @@
 import React from 'react';
 
 class App extends React.Component {
+    //create the this
+    constructor(){
+        //must start with super()
+        super();
+        //default values
+        this.state = {
+            result: 0
+        };
+        //must have this, don't know why
+        this.incrementHandler =  this.incrementHandler.bind(this);
+        this.decrementHandler =  this.decrementHandler.bind(this);
+    }
+   // the functions:
+   incrementHandler() {
+       this.setState({result: this.state.result +1});
+   }
+   decrementHandler (){
+        this.setState({result: this.state.result -1});
+   }
+
    render () {
        return (
             <div>
                <h1>Hellow world</h1>
-                <h1>{this.props.result}</h1>
-                <button> - </button>
-                <button> + </button>
+                /*on click call function*/
+                <h1>{this.state.result}</h1>
+                <button onClick={this.incrementHandler}> + </button>
+                <button onClick={this.decrementHandler}> - </button>
            </div>
        )
     };
 }
 
-App.defaultProps = {
-    result: 0
-};
+
+
 
 
 
